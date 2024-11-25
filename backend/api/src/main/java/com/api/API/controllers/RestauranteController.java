e288f0fbe858c82535985ff4440b3d0271bed563
Lucas Queiroz <lucas.fn4@gmail.com>
2024-11-25 09:43:36 -0300

    feat: adiciona endpoint para teste

--- backend/api/src/main/java/com/api/API/controllers/RestauranteController.java
+++ backend/api/src/main/java/com/api/API/controllers/RestauranteController.java
@@ -1,5 +1,13 @@
 package com.api.API.controllers;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController
 public class RestauranteController {
+
+    @GetMapping("/teste")
+    public String teste() {
+        return "ok";
+    }
 }
